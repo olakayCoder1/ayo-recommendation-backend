c8ff72778e53ec1d2eefb89e1b891b0150210cc2
olakayCoder1 <programmerolakay@gmail.com>
2025-03-09 12:02:11 +0100

    Media and static file managemnet

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0fpzqjp*jrmlh=zt7@w4!z2r84-)1rgc-yl$zx7ub04o#t3kmm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
